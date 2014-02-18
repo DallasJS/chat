f3f735758ddf9a43d1f3806396495666dab8acd1
Alexis Abril <me@alexisabril.com>
2014-02-17 21:05:01 -0600

    initial firebase example

--- client/app.js
+++ client/app.js
@@ -1,7 +1,43 @@
 var Chatbox = can.Control.extend({
 
+	defaults: {
+		messages: new can.List()
+	}
+
+}, {
+
 	init: function() {
+		var self = this;
+
+		this._fb = new Firebase('https://blistering-fire-1950.firebaseio.com');
+		this._fbAuth = new FirebaseSimpleLogin(this._fb, function(error, user) {
+			if(error && error.code === 'INVALID_USER') {
+				//user does not exist yet so we'll create it for them
+				this._fbAuth.createUser(this._email, '', function(error, user) {
+					//now that the user exists, let's log them in
+					self.login(user.email);
+				});
+			}
+			else if(user) {
+				//user has authenticated, so we'll save the user for our state
+				this._user = user;
+
+				//time to render a new view
+				this.render();
+			}
+		}, this);
+
+		this.setupListeners();
 		this.element.html(can.view('client/login.ejs', {}));
+		this._fbAuth.logout();
+	},
+
+	setupListeners: function() {
+		var self = this;
+
+		this._fb.on('child_added', function(snapshot) {
+			self.options.messages.push(snapshot.val());
+		});
 	},
 
 	render: function() {
@@ -11,31 +47,31 @@ var Chatbox = can.Control.extend({
 		}));
 	},
 
+	login: function(email) {
+		this._fbAuth.login('password', {
+			email: email,
+			password: '',
+			rememberMe: true
+		});
+	},
+
 	'.login keypress': function(el, ev) {
-		var self = this;
-
-		if(ev.charCode === 13 && el.val().trim().length) {
-			this._user = el.val();
-
-			socket.emit('users::get', function(error, users) {
-				users.forEach(function(user) {
-					self.options.users.push(user);
-				});
-			});
-
-			socket.emit('users::create', this._user, function() {});
-
-			this.render();
+		var email = el.val().toLowerCase();
+		//generic regex to validate email addresses
+		if(ev.keyCode === 13 && /\b[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\b/.test(email)) {
+			this._email = email;
+			//try and login
+			this.login(email);
 		}
 	},
 
 	'.chat keypress': function(el, ev) {
-
 		if(ev.charCode === 13) {
-			socket.emit('messages::create', {
-				text: $(ev.target).val(),
-				user: this._user
-			}, function() {});
+			this._fb.push({
+				timestamp: Date.now(),
+				text: el.val(),
+				user: this._user.email
+			});
 
 			el.val('');
 		}
@@ -43,21 +79,4 @@ var Chatbox = can.Control.extend({
 
 });
 
-var socket = io.connect('http://localhost:8000'),
-messages = new can.List([]),
-users = new can.List([]);
-
-new Chatbox('.chatbox', {
-	messages: messages,
-	users: users
-});
-
-socket.on('messages created', function(msg) {
-	var timestamp = new Date(msg.timestamp).toLocaleString();
-	msg.timestamp = timestamp;
-	messages.push(msg);
-});
-
-socket.on('users created', function(user) {
-	users.push(user);
-});
+new Chatbox('.chatbox');
